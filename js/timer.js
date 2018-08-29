c7e81a171b3931be172549a397d075328dfa5692
hausfrau <hausfrau.sunlight@gmail.com>
2018-08-29 22:20:29 +0300

    Рефакторинг подсчета ммнут и секунд.

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,16 @@
-export const getTime = (millisecs) => {
-  const secs = millisecs / 1000;
+const _getMinutes = (secs) => {
   let minutes = Math.floor(secs / 60) % 60;
-  let seconds = Math.round(secs % 60);
   minutes = minutes < 10 ? `0${minutes}` : minutes;
+  return minutes;
+};
+
+const _getSeconds = (secs) => {
+  let seconds = Math.round(secs % 60);
   seconds = seconds < 10 ? `0${seconds}` : seconds;
-  return `${minutes}:${seconds}`;
+  return seconds;
+};
+
+export const getStringTime = (millisecs) => {
+  const secs = millisecs / 1000;
+  return `${_getMinutes(secs)}:${_getSeconds(secs)}`;
 };
--- js/timer.test.js
+++ js/timer.test.js
@@ -1,20 +1,20 @@
 import {assert} from 'chai';
-import {getTime} from './timer';
+import {getStringTime} from './timer';
 
 describe(`check timer`, () => {
   it(`should return 05:00 when 300000 milliseconds`, () => {
-    assert.equal(`05:00`, getTime(300000));
+    assert.equal(`05:00`, getStringTime(300000));
   });
 
   it(`should return 05:25 when 325000 milliseconds`, () => {
-    assert.equal(`05:25`, getTime(325000));
+    assert.equal(`05:25`, getStringTime(325000));
   });
 
   it(`should return 00:00 when 0 milliseconds`, () => {
-    assert.equal(`00:00`, getTime(0));
+    assert.equal(`00:00`, getStringTime(0));
   });
 
   it(`should return 02:46 when 165912 milliseconds`, () => {
-    assert.equal(`02:46`, getTime(165912));
+    assert.equal(`02:46`, getStringTime(165912));
   });
 });
